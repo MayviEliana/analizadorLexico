2e80d4162d49a5e73d584f10122eb3a62ac48cde
MayviEliana <30846210+MayviEliana@users.noreply.github.com>
2017-10-17 00:45:00 -0500

    version 1
    primeros cambios del archivo de arbol de expresion, quitando lo de expresiones regulares y utilizando la libreria de python

---
+++ analizadorLex.py
@@ -0,0 +1,86 @@
+import re
+import sys
+from imp_lexer import *
+
+diccionario = {}
+
+class Pila:
+
+    def __init__(self):
+        self.items = []
+
+    def apilar(self, dato):
+        self.items.append(dato)
+
+    def desapilar(self):
+        return self.items.pop()
+
+    def pilaVacia(self):
+        return self.items == []
+
+
+class Nodo():
+
+    def __init__(self, valor, izq=None, der=None):
+        self.valor = valor
+        self.izq = izq
+        self.der = der
+
+
+def evaluar(arbol):
+   
+    if arbol.valor == "+":
+        return evaluar(arbol.izq) + evaluar(arbol.der)
+    if arbol.valor == "-":
+        return evaluar(arbol.izq) - evaluar(arbol.der)
+    if arbol.valor == "*":
+        return evaluar(arbol.izq) * evaluar(arbol.der)
+    if arbol.valor == "/":
+        return evaluar(arbol.izq) / evaluar(arbol.der)
+    return int(arbol.valor)
+
+
+def armarArbol(pila):
+    
+    auxPila = Pila()
+    izq, der, valor = None, None, ""
+    while not len(pila) == 0:
+
+        valor = pila.pop(0)
+       
+        if valor in "+-*/":
+            der = auxPila.desapilar()
+            izq = auxPila.desapilar()
+            auxPila.apilar(Nodo(valor, izq, der))
+            
+
+        elif valor.isdigit() == False:
+            if diccionario.has_key(valor):
+                valor = diccionario[valor][0]
+                auxPila.apilar(Nodo(valor))
+                
+            else:
+                aux = pila.pop(0)
+                
+                if aux == "=":
+                     diccionario[valor] = [evaluar(auxPila.desapilar())]
+
+        else:
+            auxPila.apilar(Nodo(valor))
+            
+def usage():
+    sys.stderr.write('Usage: imp filename\n')
+    sys.exit(1)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        usage()
+    filename = sys.argv[1]
+    text = open(filename).read()
+    tokens = imp_lex(text)
+    for token in tokens :
+        print token
+
+  #  armarArbol(tokens)
+  #  print diccionario
+        
